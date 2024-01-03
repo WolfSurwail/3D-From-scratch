31a9690ad7bce27e4f797f75a01bc252397a40aa
WolfSurwail <wolfsurwail@gmail.com>
2024-01-03 21:09:05 +0300

    03 01 2024

--- src/Screen.java
+++ src/Screen.java
@@ -2,11 +2,13 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Screen extends JPanel {
-    PolygonObject polygonObject;
+    double[] ViewFrom = new double[]{10,10,10};
+    double[] ViewTo = new double[]{0,0,0};
+    a3DPolygon a3DPolygon = new a3DPolygon(new int[]{2,4,2}, new int[]{2,4,6}, new int[]{5,5,5}, Color.BLACK);;
     public Screen() {
-        polygonObject = new PolygonObject(new int[]{10,200,10}, new int[]{10,200,400}, Color.BLACK);
+
     }
     public void paintComponent(Graphics g) {
-        polygonObject.drawPolygon(g);
+
     }
 }
---
+++ src/a3DPolygon.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class a3DPolygon {
+    Color color;
+    int[] x;
+    int[] y;
+    int[] z;
+    public a3DPolygon(int[] x, int[] y, int[] z, Color color) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.color = color;
+    }
+}
