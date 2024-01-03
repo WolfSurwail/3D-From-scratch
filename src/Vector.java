48a02e4d9d066fc8d02120302bab448f889c9e61
WolfSurwail <wolfsurwail@gmail.com>
2024-01-04 00:47:16 +0300

    04 01 2024

--- src/Vector.java
+++ src/Vector.java
@@ -1,23 +1,25 @@
-import java.awt.*;
 
 public class Vector {
-    double x = 0,y = 0,z = 0;
-    public Vector(double x, double y, double z) {
-        double length = Math.sqrt(x*x + y*y + z*z);
-        if (length > 0 ) {
-            this.x = x / length;
-            this.y = y / length;
-            this.z = z / length;
-        } else {
+    double x, y, z;
+    public Vector(double x, double y, double z)
+    {
+        double Length = Math.sqrt(x*x + y*y + z*z);
 
+        if(Length>0)
+        {
+            this.x = x/Length;
+            this.y = y/Length;
+            this.z = z/Length;
         }
+
     }
 
-    Vector CrossProduct(Vector v) {
+    Vector CrossProduct(Vector V)
+    {
         Vector CrossVector = new Vector(
-                y * v.z - z * v.y,
-                z * v.x - x * v.z,
-                x * v.y - y * v.x);
+                y * V.z - z * V.y,
+                z * V.x - x * V.z,
+                x * V.y - y * V.x);
         return CrossVector;
     }
 }
