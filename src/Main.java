cc7f762a831889c9ce60e3a521abbcbd531e2437
WolfSurwail <wolfsurwail@gmail.com>
2024-01-03 20:18:36 +0300

    03 01 2024

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ import java.awt.*;
 
 public class Main extends JFrame {
     static JFrame F = new Main();
+    Screen ScreenObject = new Screen();
     public Main() {
+        add(ScreenObject);
         setUndecorated(true);
         setSize(Toolkit.getDefaultToolkit().getScreenSize());
         setVisible(true);
