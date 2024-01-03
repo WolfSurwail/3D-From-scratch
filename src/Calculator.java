0d4f9e1140dfd8211ddd21d4544f23645f313bd4
WolfSurwail <wolfsurwail@gmail.com>
2024-01-03 23:43:31 +0300

    03 01 2024

--- src/Calculator.java
+++ src/Calculator.java
@@ -9,5 +9,8 @@ public class Calculator {
 
     static void setStuff(double[] ViewFrom, double[] ViewTo) {
         Vector ViewVector = new Vector(ViewTo[0] - ViewFrom[0],ViewTo[1] - ViewFrom[1], ViewTo[2] - ViewFrom[2]);
+        Vector DirectionVector = new Vector(1,1,1);
+        Vector PlaneVector1 = ViewVector.CrossProduct(DirectionVector);
+        Vector PlaneVector2 = ViewVector.CrossProduct(PlaneVector1);
     }
 }
--- src/Vector.java
+++ src/Vector.java
@@ -1,8 +1,19 @@
+import java.awt.*;
+
 public class Vector {
     double x,y,z;
     public Vector(double x, double y, double z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+        double length = Math.sqrt(x*x + y*y + z*z);
+        this.x = x/length;
+        this.y = y/length;
+        this.z = z/length;
+    }
+
+    Vector CrossProduct(Vector v) {
+        Vector CrossVector = new Vector(
+                y * v.z - z * v.y,
+                z * v.x - x * v.z,
+                x * v.y - y * v.x);
+        return CrossVector;
     }
 }
