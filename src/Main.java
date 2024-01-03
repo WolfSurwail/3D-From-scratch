0b8170ef520a48dba2bd13fc286e8403ba71de31
WolfSurwail <wolfsurwail@gmail.com>
2024-01-03 20:16:41 +0300

    03 01 2024

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+    static JFrame F = new Main();
+    public Main() {
+        setUndecorated(true);
+        setSize(Toolkit.getDefaultToolkit().getScreenSize());
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+
+    }
+}
